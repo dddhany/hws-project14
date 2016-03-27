a31bdbf6dc58af21b2c9be851a3fb389813f2a5e
Dhany Basuki <dddhany+github@gmail.com>
2016-03-27 11:53:33 +0800

    first commit

---
+++ project14/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  project14
+//
+//  Created by Dhany Basuki on 27/3/16.
+//  Copyright (c) 2016 Dhany. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
